805b45788b74907d82ae10de118919c2dfc00ad0
lain <xl.finance@outlook.com>
2023-03-20 22:00:31 +0800

    progressing

---
+++ src/orderMgt/OrderMgt.py
@@ -0,0 +1,6 @@
+# orderMgt module
+class OrderMgt:
+    def __init__(self, symbol, timeFrame):
+        self.symbol = symbol
+        self.timeFrame = timeFrame
+        # self.get_available_intervals()
