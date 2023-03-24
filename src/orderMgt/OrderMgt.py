2e5f3fa70e8574bf2706b5903040a678533d873d
lain <xl.finance@outlook.com>
2023-03-24 22:55:39 +0800

    processing orderMgt

--- src/orderMgt/OrderMgt.py
+++ src/orderMgt/OrderMgt.py
@@ -1,6 +1,24 @@
 # orderMgt module
-class OrderMgt:
-    def __init__(self, symbol, timeFrame):
-        self.symbol = symbol
-        self.timeFrame = timeFrame
-        # self.get_available_intervals()
+
+class OrderManagement:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.orders = []
+
+    def add_order(self, order):
+        self.orders.append(order)
+
+    def remove_order(self, order):
+        self.orders.remove(order)
+
+    def get_orders(self):
+        return self.orders
+    
+    # create stop orders
+    def 
